fc4f9c7ddc6d669135af16c3b7730ef0f8af1d3c
VAOkolo <vincentaokolo@outlook.com>
2022-08-03 17:23:29 +0100

    debugged

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import { Container, Row, Col } from "react-bootstrap/";
 import { UserDisplay, RepoDisplay, People, TopLanguages } from "../../components";
@@ -10,16 +10,22 @@ import { useSelector } from 'react-redux';
 const Home = () => {
 
   const userObject = useSelector(state => state.userObject);
-  console.log('i am the user object in the the home pagae', userObject)
+  console.log(userObject)
+  // console.log('i am the user object in the the home pagae', userObject)
   const followers = useSelector(state => state.followers);
   // console.log('userobject', userObject);
   // console.log('home pagae followers', followers);
 
+  let login = ""
+  let profilePhoto = ""
+  let weblink = ""
 
-  // const login = userObject[0].owner.login
-  // const profilePhoto = userObject[0].owner.avatar_url
-  // const weblink = userObject[0].owner.url
-
+  if(Object.keys(userObject).length !== 0){
+    login = userObject[0].owner.login
+    profilePhoto = userObject[0].owner.avatar_url
+    weblink = userObject[0].owner.url
+  }
+    
 
 
   return (
@@ -28,16 +34,16 @@ const Home = () => {
       <Row>
         {/* displayer user info / profile */}
         <Col sm={12}>
-          {/* <UserDisplay login={login} profilePhoto={profilePhoto} weblink={weblink} /> */}
+          <UserDisplay login={login} profilePhoto={profilePhoto} weblink={weblink} />
         </Col>
       </Row>
       {/* This is the Second row which renders repo and people section. On small screens every column is 12units wide - so effectively a row -  but on medium and above they will break into columns*/}
       <Row className="align-items-start">
         {/* display repos section */}
         <Col sm={12} md={9} xl={10}>
-          {/* {userObject ? userObject.map((item) => (
+          {Object.keys(userObject).length !== 0 ? userObject.map((item) => (
             <RepoDisplay key={item.id} name={item.name} description={item.description} stargazers_count={item.stargazers_count} forks_count={item.forks_count} open_issues={item.open_issues} updated_at={item.updated_at}/>
-          )) : <div>hiiiii </div>} */}
+          )) : <div>hiiiii </div>}
         </Col>
              {/* display people section */}
         <Col sm={12} md={3} xl={2}>
