0af4cd2b4b58bdb8b066e10c049ea2663cbc0d3f
VAOkolo <vincentaokolo@outlook.com>
2022-08-04 17:21:35 +0100

    netlify debug - commented favicon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import Favicon from 'react-favicon';
+// import Favicon from 'react-favicon';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter> 
-    <Favicon url="https://cdn-icons-png.flaticon.com/512/5968/5968866.png" />
+    {/* <Favicon url="https://cdn-icons-png.flaticon.com/512/5968/5968866.png" /> */}
     <Provider store={store}>  
         <App />     
         </Provider>    
