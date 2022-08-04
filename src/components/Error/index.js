027d9646438acef151193cde70a27c1aa1b26d4a
VAOkolo <vincentaokolo@outlook.com>
2022-08-04 11:20:21 +0100

    edit - footer fixed to bottom of page / github and search bar added

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -6,22 +6,46 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 const Error = () => {
   
+//   return (
+//     <Container fluid >
+//  <Row>
+//  <Col sm={12}>
+//    <Col sm={12} className='my-2 bg-black text-light text-start ' border="secondary">
+//   <Figure>
+//       <Figure.Image
+//         width={500}
+//         height={500}
+//         alt="171x180"
+//         src="https://cdn-media-1.freecodecamp.org/images/1*FFYtzf28XKPFBdknfXf-jg.jpeg"
+//       />
+//       <Figure.Caption>
+//         Nulla vitae elit libero, a pharetra augue mollis interdum.
+//       </Figure.Caption>
+//     </Figure>
+//   </Col>
+//  </Col>
+ 
+//  </Row>
+  
+//     </Container>
+   
+  
+//   )
+
   return (
     <Container fluid >
  <Row>
  <Col sm={12}>
    <Col sm={12} className='my-2 bg-black text-light text-start ' border="secondary">
-  <Figure>
-      <Figure.Image
-        width={500}
-        height={500}
-        alt="171x180"
-        src="https://cdn-media-1.freecodecamp.org/images/1*FFYtzf28XKPFBdknfXf-jg.jpeg"
-      />
-      <Figure.Caption>
-        Nulla vitae elit libero, a pharetra augue mollis interdum.
-      </Figure.Caption>
-    </Figure>
+   <div className="searchDiv">
+       <div className="githubTitle">
+         <h1>Github</h1>
+       </div>
+       <div className="bar">
+         <input className="searchBar" type="text" title="Search"></input>
+         <button className="searchButton">Search</button>
+       </div>
+     </div>
   </Col>
  </Col>
  
@@ -31,6 +55,22 @@ const Error = () => {
    
   
   )
+
+  // return (
+  //   <>
+  //   <div className="searchDiv">
+  //     <div className="githubTitle">
+  //       <h1>Github</h1>
+  //     </div>
+  //     <div className="bar">
+  //       <input className="searchBar" type="text" title="Search"></input>
+  //       <button className="searchButton">Search</button>
+  //     </div>
+  //   </div>
+  //   </>
+  // )
+
+
 }
 
 export default Error
