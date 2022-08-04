db5b944b243b3bad5af2ee958baef54ebbc920b0
VAOkolo <vincentaokolo@outlook.com>
2022-08-04 12:11:39 +0100

    search bar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages'
+import { Home, Lander } from './pages'
 import Layout from './layout';
 import { useSelector } from 'react-redux';
 
@@ -16,7 +16,8 @@ function App() {
 
     <Routes> 
       <Route path='/' element={<Layout />}>  
-          <Route path='/' element={<Home />} />
+          <Route path="/" element={<Lander />} />
+          <Route path='/searched' element={<Home />} />
           <Route path='*' element={<Home />} />
       </Route>
     </Routes> 
---
+++ src/pages/Lander/index.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Col, Container, Figure, Image, Row } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+import { useEffect, useState} from "react";
+import { useDispatch } from "react-redux";
+import { getFollowers, getResult } from "../../actions";
+import { useNavigate } from 'react-router-dom'
+
+const Lander = () => {
+  
+  const [searchTerm, setSearchTerm] = useState("");
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("i am event value", searchTerm);
+    dispatch(getResult(searchTerm));
+    navigate('/searched', { replace: true })
+    // dispatch(getFollowers(searchTerm))
+  };
+  const updateInput = (e) => {
+    e.preventDefault();
+    const input = e.target.value;
+    setSearchTerm(input);
+  };
+
+  useEffect(() => {
+    // setSearchTerm("github");
+  }, []);
+
+  return (
+    <Container fluid >
+ <Row>
+ <Col sm={12}>
+   <Col sm={12} className='my-2 bg-black text-light text-start ' border="secondary">
+   <div className="searchDiv">
+       <div className="githubTitle">
+         <h1>Github</h1>
+       </div>
+       <div className="bar">
+         <form onSubmit={handleSubmit}>
+         <input onChange={updateInput} className="searchBar" type="text" title="Search" value={searchTerm}></input>
+         <button className="searchButton">Search</button>
+         </form>
+       </div>
+     </div>
+  </Col>
+ </Col>
+ 
+ </Row>
+  
+    </Container>
+   
+  )
+}
+
+export default Lander
